65ebb5052e5cef1f9b30203841b86d2a18066313
Oliver Bates <oliver.bates@gmail.com>
2024-05-13 09:22:40 +0100

    mergy mergey

--- generate_card_path.py
+++ generate_card_path.py
@@ -1,10 +1,26 @@
 import os
+import string
 from fnmatch import fnmatch
+from natsort import natsorted
+import csv
+
 
 root = './cards'
 pattern = "*.png"
+url = "https://github.com/oscarechobravo/mealdealv1_cards/tree/main"
 
-for path, subdirs, files in os.walk(root):
-    for name in files:
-        if fnmatch(name, pattern):
-            print(os.path.join(path, name))
+header = ["label","image","item-count","item-key"]
+
+with open('cards_v1.csv', 'w', newline='') as csvfile:
+    spamwriter = csv.writer(csvfile, delimiter=',', quotechar='"', quoting=csv.QUOTE_ALL)
+    spamwriter.writerow(header)
+
+    for path, subdirs, files in os.walk(root):
+        subdirs.sort()
+        for name in natsorted(files):
+            if fnmatch(name, pattern):
+                fname = (os.path.join(url,os.path.join(path, name)).replace("/./","/"))
+                
+                spamwriter.writerow([name, fname, 0, name])
+
+            
